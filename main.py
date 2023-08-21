89d36fb2a00e3052f2554a01563dc85a8b679ca0
Matheus Shiokawa Silva <matheus.pedro84@hotmail.com>
2023-08-20 21:22:51 -0300

    segundo commit
    estilizei a primeira tabela do programa, criei a função de limpar os campos e comentei a criação do banco de dados e de uma da tabela de ticker e amount, ao entrar devo criar as treeviews de comentários e histórico de proventos antes de criar as tabelas.

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from tkinter import *
+from tkinter import ttk
+import sqlite3
 
 window = Tk()
 
-class Aplication():
+class Functions():
+    #função para limpar os campos
+    def clean_entries(self): 
+        self.ticker_entry.delete(0, END)
+        self.amount_entry.delete(0, END)
+        self.comments_entry.delete(0, END)
+    
+    #função para se conectar aos bancos de dados
+    '''
+    def bd_connect(self): 
+        self.connect = sqlite3.connect('stocks.bd')
+        self.cursor = self.connect.cursor()
+    
+    #função para se desconectar dos bancos de dados
+    def bd_disconnect(self): 
+        self.connect.close()
+
+    #função para criar as tabelas
+    def create_table(self):
+        self.bd_connect()
+        self.cursor.execute("""
+            create table if not exists tracker_amount (
+                            ticker not null varchar(6),
+                            aumont int,
+                            primary key (ticker)
+            ) default charset utf8mb4;""")
+    '''
+
+
+class Aplication(Functions):
     def __init__(self):
         self.window = window
         self.screen_settings()
@@ -10,6 +41,7 @@ class Aplication():
         self.buttons()
         self.texts()
         self.entries()
+        self.ticker_aumont_table()
         window.mainloop()
     
     def screen_settings(self):
@@ -35,7 +67,8 @@ class Aplication():
         self.new_button = Button(self.window, text='Add', bg='#800020', fg='white', bd=3, font=('garamond', 11, 'bold'))
         self.new_button.place(relx=0.02,rely=0.46,relwidth=0.05,relheight=0.03)
 
-        self.clean_button = Button(self.window, text='Clean', bg='#800020', fg='white', bd=3, font=('garamond', 11, 'bold'))
+        self.clean_button = Button(self.window, text='Clean', bg='#800020', fg='white', bd=3, 
+                                   font=('garamond', 11, 'bold'), command=self.clean_entries)
         self.clean_button.place(relx=0.09,rely=0.46,relwidth=0.05,relheight=0.03)
 
         self.delete_button = Button(self.window, text='Delete', bg='#800020', fg='white', bd=3, font=('garamond', 11, 'bold'))
@@ -46,7 +79,7 @@ class Aplication():
 
     def texts(self):
         self.ticker_text = Label(self.window, text='Ticker', bg='#880808',fg='white', font=('garamond', 13, 'bold'))
-        self.ticker_text.place(relx=0.008,rely=0.245, relwidth=0.05, relheight=0.025)
+        self.ticker_text.place(relx=0.008,rely=0.255, relwidth=0.05, relheight=0.025)
 
         self.amount_text = Label(self.window, text='Amount', bg='#880808',fg='white', font=('garamond', 13, 'bold'))
         self.amount_text.place(relx=0.012,rely=0.32, relwidth=0.05, relheight=0.025)
@@ -55,13 +88,31 @@ class Aplication():
         self.comments_text.place(relx=0.017,rely=0.385, relwidth=0.05, relheight=0.025)
 
     def entries(self):
-        self.ticker_entry = Entry(self.window)
+        self.ticker_entry = Entry(self.window, font=('garamond', 13))
         self.ticker_entry.place(relx=0.02,rely=0.28,relwidth=0.05,relheight=0.025)
 
-        self.amount_entry = Entry(self.window)
+        self.amount_entry = Spinbox(self.window, from_=0, to=100000000000000, font=('garamond', 13))
         self.amount_entry.place(relx=0.02,rely=0.345,relwidth=0.05,relheight=0.025)
 
-        self.amount_entry = Entry(self.window)
-        self.amount_entry.place(relx=0.02,rely=0.41,relwidth=0.26,relheight=0.025)
+        self.comments_entry = Entry(self.window, font=('garamond', 13))
+        self.comments_entry.place(relx=0.02,rely=0.41,relwidth=0.26,relheight=0.025)
+
+    def ticker_aumont_table(self):
+        self.ticker_amount_table = ttk.Treeview(self.frame_tabela_ticker_amount, height= 3, columns=('column1', 'column2'))
+        self.ticker_amount_table.configure(height=5, show='headings')
+        self.ticker_amount_table.heading('#1', text='Ticker')
+        self.ticker_amount_table.heading('#2', text='Aumont')
+
+        self.ticker_amount_table.column('#1', width=10)
+        self.ticker_amount_table.column('#2', width=10)
+
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("garamond", 12, "bold"))
+        self.ticker_amount_table.place(relx=0,rely=0,relwidth=0.95,relheight=1)
+
+        self.scrool_ticker_amount_table = Scrollbar(self.frame_tabela_ticker_amount, orient='vertical')
+        self.ticker_amount_table.configure(yscroll=self.scrool_ticker_amount_table.set)
+        self.scrool_ticker_amount_table.place(relx=0.94, rely=0.001, relwidth=0.06, relheight=0.9999)
+
         
 Aplication()
